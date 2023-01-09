0f49a4c4451b1bbbf7f9fed89908ce76d1c4da8d
Alberto Venturini <aventurini@gmail.com>
2023-01-09 11:05:47 +0200

    Set since-build to IJ 2022.3 to ensure full compatibility with Java 17 (#49)

--- ui/jetbrains/src/main/java/com/albertoventurini/graphdbplugin/jetbrains/component/datasource/metadata/neo4j/Neo4jIndexMetadata.java
+++ ui/jetbrains/src/main/java/com/albertoventurini/graphdbplugin/jetbrains/component/datasource/metadata/neo4j/Neo4jIndexMetadata.java
@@ -1,7 +1,5 @@
 package com.albertoventurini.graphdbplugin.jetbrains.component.datasource.metadata.neo4j;
 
-import java.util.List;
-
 public record Neo4jIndexMetadata(
         String name,
         String state) { }
